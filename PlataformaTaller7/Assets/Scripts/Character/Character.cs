0a0ea5f9cd0bc03f3f1ceb656cfcd1795ff69966
JuanD2011 <juan.atehortual@upb.edu.co>
2019-10-11 18:27:48 -0500

    Dice

---
+++ PlataformaTaller7/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private bool canMove = false;
+
+    public Sprite CharacterSprite { get; set; }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+
+    }
+}
--- PlataformaTaller7/Assets/Scripts/Ladder/Dice.cs
+++ PlataformaTaller7/Assets/Scripts/Ladder/Dice.cs
@@ -1,14 +1,46 @@
 ﻿using UnityEngine;
+using System.Collections;
+using Delegates;
 
 public class Dice : MonoBehaviour
 {
-    public void Throw()
+    private new SpriteRenderer renderer;
+    private Sprite[] diceSprites;
+    private bool canThrow = false;
+
+    private int currentNumber = 0;
+
+    public event Action<int> OnDiceResult = null;
+
+    private void Awake()
     {
-        GetRandomNumber();
+        renderer = GetComponent<SpriteRenderer>();
+        diceSprites = Resources.LoadAll<Sprite>("Dice Sprites/");
+        renderer.sprite = diceSprites[5];
+        canThrow = true;
     }
 
-    private int GetRandomNumber()
+    private void OnMouseDown()
     {
-        return Random.Range(1, 7);
+        if (canThrow) StartCoroutine(Throw());
     }
+
+    private IEnumerator Throw()
+    {
+        SetCanThrow(false);
+        int randomDiceSide = 0;
+
+        for (int i = 0; i < 20; i++)
+        {
+            randomDiceSide = Random.Range(0, 6);
+            renderer.sprite = diceSprites[randomDiceSide];
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        currentNumber = randomDiceSide + 1;
+        SetCanThrow(true);
+        OnDiceResult?.Invoke(currentNumber);
+    }
+
+    public void SetCanThrow(bool _value) => canThrow = _value;
 }
