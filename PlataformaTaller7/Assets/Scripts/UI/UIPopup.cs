9640bf9de55da9997690afa53fe8b629776e7841
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-10-25 17:34:02 -0500

    Profile Popup[WIP]

---
+++ PlataformaTaller7/Assets/Scripts/UI/UIPopup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class UIPopup : MonoBehaviour
+{
+    [SerializeField] string popupIn = "", popupOut = "";
+
+    private Animator animator = null;
+
+    private bool isIn = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Change states between in or out
+    /// </summary>
+    /// <param name="_Value"></param>
+    public void PopupAnimation()
+    {
+        if (!isIn)
+        {
+            animator.Play(popupIn);
+            isIn = true;
+        }
+        else
+        {
+            animator.Play(popupOut);
+            isIn = false;
+        } 
+    }
+}
