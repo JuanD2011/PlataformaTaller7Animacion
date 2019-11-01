105871532fa1f449a9fbfed56799f6d719b5e011
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-11-01 11:32:54 -0500

    Victory Condition UI

--- PlataformaTaller7/Assets/Scripts/UI/MenuGameManager.cs
+++ PlataformaTaller7/Assets/Scripts/UI/MenuGameManager.cs
@@ -7,6 +7,9 @@ public class MenuGameManager : MonoBehaviour
     [SerializeField]
     private Animator chestAnimator = null;
 
+    [SerializeField]
+    private Animator victoryAnimation = null;
+
     private void Awake()
     {
         settingsTabManager = GetComponent<SettingsTabManager>();
@@ -15,11 +18,18 @@ public class MenuGameManager : MonoBehaviour
     private void Start()
     {
         LadderManager.Manager.Character.OnReachDestination += ActiveQuestionPanel;
+        //Receive win condition
     }
 
     private void ActiveQuestionPanel(BoxType _boxType)
     {
         settingsTabManager.ModalAnim(true);
         chestAnimator.Play("Popup Window In");
+    }
+
+    private void ActiveVictoryPanel()
+    {
+        settingsTabManager.ModalAnim(true);
+        victoryAnimation.Play("Popup Window In");
     }
 }
