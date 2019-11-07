9af8ab694c6a14392392beb73cbe72030a830d03
JuanD2011 <juan.atehortual@upb.edu.co>
2019-11-06 19:54:23 -0500

    Line fixed

--- PlataformaTaller7/Assets/Scripts/UI/Association/UIAssociationLine.cs
+++ PlataformaTaller7/Assets/Scripts/UI/Association/UIAssociationLine.cs
@@ -28,7 +28,7 @@ public class UIAssociationLine : MonoBehaviour
         rect.position = _FirstPoint;
         Debug.DrawLine(rect.position, _SecondPoint, Color.red, 5f);
         rect.up = _SecondPoint - rect.position;
-        targetHeight = Mathf.Sqrt((_SecondPoint - _FirstPoint).sqrMagnitude) * 1.05f;
+        targetHeight = Vector2.Distance(_SecondPoint, rect.position) / 2f;
 
         StartCoroutine(LerpLine());
     }
